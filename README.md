5a76700197f1259ad7195eefdc16e19e6ecb40cd
AnotherZane <zanedpereira2@gmail.com>
2020-08-07 16:30:51 +0530

    Added setup script

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # MCP-Patcher
-A patch generation tool for MCP-Reborn
+
+#### A patch creation tool for MCP.
+
+### Prerequisites
+
+- ``git``
+
+- ``patch``
+
+- ``unrar`` (Required to add optifine)
+
+### Setup:
+
+- Make ``mcp-patcher`` file executable with command: ``chmod +x mcp-patcher``.
+
+- Run the setup with command: ``./mcp-patcher setup`` (Right click and run with git bash on Windows, and type setup when prompted).
+
+- Import the build.gradle in Intellij.
+
+- Run the gradle task 'setup' in the mcp folder.
+
+### Patcher Usage:
+
+- Once you have mcp setup, run ``./mcp-patcher patch`` to apply all previous patches.
+
+- After making changes, run ``./mcp-patcher rebuild`` to rebuild patches with differences.
+
+### How to build:
+
+- Run the gradle task 'build', then the release jar will be in the "build/libs/".
+
+### Credits:
+
+* [MCP-Reborn](https://github.com/Hexeption/MCP-Reborn) for a working MCP ([Hexeption](https://github.com/Hexeption), [kingdevnl](https://github.com/kingdevnl))
+
+* [Optifine-SRC](https://github.com/Hexeption/Optifine-SRC) for optifine source code ([Hexeption](https://github.com/Hexeption))
+
+* [Paper](https://github.com/PaperMC/Paper) for parts of patch scripts ([PaperMC Team](https://github.com/PaperMC))
+
+* [MinecraftForge](https://github.com/MinecraftForge/MinecraftForge) for MCPConfig, ForgeGradle ([MinecraftForge Team](https://github.com/MinecraftForge))
---
+++ scripts/setup.sh
@@ -0,0 +1,69 @@
+#!/usr/bin/env bash
+
+OPTF_URL="https://github.com/AnotherZane/Optifine-SRC/raw/master/Optifine%20SRC%20Version%20%5B1.16.1%20HD%20U%20G2%5D.rar"
+GITIGNORE_URL="https://gist.githubusercontent.com/AnotherZane/66e804e79b8a4d2e885c0a82ff53c682/raw/a08334633e08fa0b9d6717a153acbfce7b40f948/MCP-Reborn%2520.gitignore"
+
+if [[ -d Client/ ]]
+then
+    echo "Client folder (Client/) already exists!"
+    echo "Delete it if you want to setup the client again."
+
+else
+    echo "Cloning MCP-Reborn files into Client folder..."
+    git clone https://github.com/AnotherZane/MCP-Reborn.git Client
+    cd Client/
+
+    echo ""
+    echo "Setting up MCP... (This may take a while)"
+    echo ""
+    chmod +x gradlew
+    ./gradlew setup
+    mkdir src/main/resources
+
+    echo ""
+    echo "Cleaning up..."
+    rm -rf .github/
+
+    echo "Do you require Optifine? [y/n]"
+    read optf
+
+    if [ $optf == "y" ]
+    then
+        echo "Downloading Optifine..."
+        wget $OPTF_URL -O optfine.rar
+
+        echo "Uncompressing..."
+        mkdir tmp
+        unrar x optfine.rar tmp/
+
+        echo "Copying files..."
+        cp tmp/assets/ src/main/resources/ -rf
+        cp tmp/com/ src/main/java/ -rf
+        cp tmp/mcp/ src/main/java/ -rf
+        cp tmp/net/ src/main/java/ -rf
+        cp tmp/Start.java src/main/java/ -f
+        
+        echo "Cleaning up..."
+        rm -rf tmp/
+        rm -f optfine.rar
+
+        echo "Added Optifine!"
+    else
+        echo "Skipped adding Optfine..."
+    fi
+
+    echo "Re-initializing git repo..."
+    rm -rf .git
+    git init
+
+    echo "Modifying .gitignore"
+    rm -f .gitignore
+    wget $GITIGNORE_URL -O .gitignore
+
+    echo "Creating first commit... (This commit will be ignored by the patch creator as it contains mc source code.)"
+    git add .
+    git commit -m "Minecraft Source"
+
+    echo ""
+    echo "Done!"
+fi
